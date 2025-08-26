4ca186aaf671302bc91973084d1cae5f63ff3170
Christian Benincasa <chrisbenincasa@gmail.com>
2025-08-26 13:03:14 -0400

    feat: use official semver v2 regex to parse version

--- src/windUpJob.task.js
+++ src/windUpJob.task.js
@@ -1,5 +1,8 @@
-const core = require('@actions/core');
-const outputs = require('./outputs.json');
+const core = require("@actions/core");
+const outputs = require("./outputs.json");
+
+const semverRegex =
+  /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
 
 /**
  * windUpJob
@@ -8,11 +11,11 @@ const outputs = require('./outputs.json');
  */
 module.exports = async (result) => {
   if (!result) {
-    core.debug('No release published.');
+    core.debug("No release published.");
     return Promise.resolve();
   }
 
-  const {lastRelease, commits, nextRelease, releases} = await result;
+  const { lastRelease, commits, nextRelease, releases } = await result;
 
   if (lastRelease.version) {
     core.debug(`The last release was "${lastRelease.version}".`);
@@ -22,27 +25,34 @@ module.exports = async (result) => {
   }
 
   if (!nextRelease) {
-    core.debug('No release published.');
+    core.debug("No release published.");
     return Promise.resolve();
   }
 
-  core.debug(`Published ${nextRelease.type} release version ${nextRelease.version} containing ${commits.length} commits.`);
+  core.debug(
+    `Published ${nextRelease.type} release version ${nextRelease.version} containing ${commits.length} commits.`
+  );
 
   for (const release of releases) {
-    core.debug(`The release was published with plugin "${release.pluginName}".`);
+    core.debug(
+      `The release was published with plugin "${release.pluginName}".`
+    );
   }
 
-  const {version, channel, notes, gitHead, gitTag} = nextRelease;
-  const [major, minor, patch] = version.split(/\.|-|\s/g, 3);
+  const { version, channel, notes, gitHead, gitTag } = nextRelease;
+  const [_, major, minor, patch, prerelease, buildmetadata] =
+    version.match(semverRegex);
 
   // set outputs
-  core.setOutput(outputs.new_release_published, 'true');
+  core.setOutput(outputs.new_release_published, "true");
   core.setOutput(outputs.new_release_version, version);
   core.setOutput(outputs.new_release_major_version, major);
   core.setOutput(outputs.new_release_minor_version, minor);
   core.setOutput(outputs.new_release_patch_version, patch);
+  core.setOutput(outputs.new_release_prerelease_version, prerelease);
+  core.setOutput(outputs.new_release_buildmetadata, buildmetadata);
   core.setOutput(outputs.new_release_channel, channel);
-  core.setOutput(outputs.new_release_notes, notes);  
+  core.setOutput(outputs.new_release_notes, notes);
   core.setOutput(outputs.new_release_git_head, gitHead);
   core.setOutput(outputs.new_release_git_tag, gitTag);
 };
